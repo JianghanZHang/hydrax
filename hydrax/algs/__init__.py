8cf7df0773825e4e49e7eb706e02efae6dc3173e
JianghanZHang <jz5480@nyu.edu>
2025-05-29 15:19:46 -0400

    Added new algs

--- hydrax/algs/__init__.py
+++ hydrax/algs/__init__.py
@@ -2,5 +2,9 @@ from .cem import CEM
 from .evosax import Evosax
 from .mppi import MPPI
 from .predictive_sampling import PredictiveSampling
+from .xnes import xNES
+from .cmaes import CMAES
+from .sves import SVES
+from .gs import GaussianSmoothing
 
-__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax"]
+__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax", "xNES", "CMAES", "SVES", "GaussianSmoothing"]
