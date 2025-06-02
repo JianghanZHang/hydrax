204b2da5e70a7cce2301d8712805713c22019ac0
JianghanZHang <jz5480@nyu.edu>
2025-06-02 14:03:44 -0400

    Added new classes

--- hydrax/algs/__init__.py
+++ hydrax/algs/__init__.py
@@ -6,5 +6,6 @@ from .xnes import xNES
 from .cmaes import CMAES
 from .sves import SVES
 from .gs import GaussianSmoothing
+from .xmppi import xMPPI
 
-__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax", "xNES", "CMAES", "SVES", "GaussianSmoothing"]
+__all__ = ["CEM", "MPPI", "PredictiveSampling", "Evosax", "xNES", "CMAES", "SVES", "GaussianSmoothing", "xMPPI"]
